dcbf5759e0ef79f65033c4e5d9448bf7d71ed0eb
PatrikKarkkainen <karkkainenpatrik@gmail.com>
2021-11-22 16:47:13 +0200

    Update on get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -4,20 +4,53 @@
 #include <unistd.h>
 #include "get_next_line.h"
 #include <string.h>
-#include <stdlib.h>
 
 int	get_next_line(const int fd, char **line)
 {
-	char	buf[BUF_SIZE + 1];
+	int		ret;
+//	const char	*ch[BUFF_SIZE];
+	char		buf[BUFF_SIZE + 1];
 
+	if (fd < 0)
+		return (-1);
 	if (!*line)
 		*line = (char *)malloc(sizeof(char) * 50);
-
-	while (read(fd, buf, 1) > 0)
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
+		buf[ret] = '\0';
 		if (buf[0] == '\n')
-			return (0);
-		strncat(*line, buf, 1);
+			return (1);
+		*line = ft_strdup(buf);
 	}
-	return (0);	
+	return (1);
+}
+
+char	*ft_strdup(const char *string)
+{
+	int		i;
+	int		len;
+	char	*str;
+
+	i = 0;
+	len = ft_strlen(string);
+	str = (char *)malloc((len + 1) * sizeof(char));
+	while (string[i] != '\0')
+	{
+		str[i] = string[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,6 +1,10 @@
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
-# define BUF_SIZE 32
+# define BUFF_SIZE 32
+
+#include <stdlib.h>
 
 int	get_next_line(const int fd, char **line);
+char    *ft_strdup(const char *string);
+size_t  ft_strlen(const char *str);
 #endif
